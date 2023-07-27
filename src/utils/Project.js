9519b06217be80f48b1d951e7e10652dbc2660fe
Adeyera <asimolowoelizabeth@gmail.com>
2023-07-27 08:11:44 +0100

    Added a file

--- src/utils/Project.js
+++ src/utils/Project.js
@@ -1,6 +1,7 @@
 import gpt3 from '../assets/gpt3.png';
 import loanify from '../assets/loanify.png';
 import starWars from '../assets/starWars.png';
+import bestEat from '../assets/bestEat.png';
 
 
 const project = [
@@ -14,6 +15,14 @@ const project = [
     },
     {
         id: 2,
+        title: 'Best Eat Restaurant',
+        description: 'Best Eat is a landing page for a modern restaurant with varieties of foods',
+        image: bestEat,
+        demoLink: 'https://tubular-fudge-599ab0.netlify.app/',
+        githubLink: 'https://github.com/Adeyera16/Best_eat-Restaurant',
+    },
+    {
+        id: 3,
         title: 'GPT3',
         description: 'This is a landing page of the GPT3 app',
         image: gpt3,
@@ -21,7 +30,7 @@ const project = [
         githubLink: 'https://github.com/Adeyera16/Gpt3_zay',
     },
     {
-        id: 3,
+        id: 4,
         title: 'Starwars',
         description: 'This is an app that integrates with the starwars API to deplay a list of movies',
         image: starWars,
