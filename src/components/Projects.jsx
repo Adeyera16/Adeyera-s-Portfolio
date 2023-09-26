b4910ebe5d059d0c39c52d848cb71228a769f621
Adeyera <asimolowoelizabeth@gmail.com>
2023-09-26 17:34:05 +0100

    Added changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
   return (
      <div id='projects' className='w-full py-16 '>
          <div className=' text-center'>
-         <h1 className='text-[40px] font-extrabold  text-black'>Projects</h1>
+         <h1 className='text-[40px] font-extrabold  gradient__text'>Projects</h1>
           <p className=' py-8 px-5 text-gray-500 max-w-[800px] mx-auto '>These projects serve as tangible examples of my skills and expertise, displaying real-world examples of my work. They include brief descriptions as well as links to code repositories and live demos. Through these projects, I demonstrate my ability to deal with complex problems, adapt to new technologies, and effectively manage project development.</p>
          </div>
          <div className='grid md:grid-cols-3 gap-12 mx-[5rem] mt-[3rem] text-gray-500'>
